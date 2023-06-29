ded83665c7bfeb14e78c2b4136dbc6ea34fa1d49
Ajay manjhi <120018823+AjayManjhi09@users.noreply.github.com>
2023-06-29 13:32:16 +0530

    Update script.js

--- Passwaord_gernetor/script.js
+++ Passwaord_gernetor/script.js
@@ -1,4 +1,3 @@
-// ajayyyyyyyyyyyyyyyyyyyyyy
 const inputSlider = document.querySelector("[data-lengthSlider]");
 const lengthDisplay = document.querySelector("[data-lengthNumber]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
